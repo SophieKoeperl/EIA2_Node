e3ea92243c5c186905f43c3b5072f6fe857e48bb
sophie <sophie@sophies-mbp-2.hfu-open.fu.hs-furtwangen.de>
2018-06-14 10:26:23 +0200

    database

--- Database.ts
+++ Database.ts
@@ -45,11 +45,11 @@ export function findAll(_callback: Function): void {
         } else {
             let line: string;
             for (let i: number = 0; i < studentArray.length; i++) {
-            line = studentArray[i].matrikel + ": " + studentArray[i].studiengang + ", " + studentArray[i].name + ", " + studentArray[i].firstname + ", " + studentArray[i].age + ", "; 
+            line = studentArray[i].matrikel + " : " + studentArray[i].studiengang + ", " + studentArray[i].name + ", " + studentArray[i].firstname + ", " + studentArray[i].age + ", "; 
             line += studentArray[i].gender ? "(M)" : "(F)";
             line += "\n";
             }
-            _callback(line);
+            _callback(line); 
         }
     }
 }
